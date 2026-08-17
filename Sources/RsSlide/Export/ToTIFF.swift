9fe883521be1e847bca36dd4d39fa46cb9b92e6f
rayman-zhao <zhao_yu@me.com>
2026-08-17 15:09:40 +0800

    Optimize tiff crop

--- Sources/RsSlide/Export/ToTIFF.swift
+++ Sources/RsSlide/Export/ToTIFF.swift
@@ -4,12 +4,6 @@ import RsFoundation
 
 extension Slide {
     func crop(rect: CGRect, toTIFF url: URL) throws {
-        let roiWidth = Int(ceil(rect.width))
-        let roiHeight = Int(ceil(rect.height))
-        let stripHeight = tileTrait.size.h
-        let pixelBytes = tileTrait.pixelBytes
-        let rowStride = roiWidth * pixelBytes
-
         #if os(Windows)
             let tiff = TIFFOpenW(url.filePath.wideString, "w")
         #else
@@ -18,16 +12,18 @@ extension Slide {
         guard let tiff else { throw SlideExportError.failedToCreateFile(url: url) }
         defer { TIFFClose(tiff) }
 
-        _ = TIFFSetField(tiff, TIFFTAG_IMAGEWIDTH, UInt32(roiWidth))
-        _ = TIFFSetField(tiff, TIFFTAG_IMAGELENGTH, UInt32(roiHeight))
+        let stripHeight = tileTrait.size.h
+
+        _ = TIFFSetField(tiff, TIFFTAG_IMAGEWIDTH, UInt32(rect.width))
+        _ = TIFFSetField(tiff, TIFFTAG_IMAGELENGTH, UInt32(rect.height))
         _ = TIFFSetField(tiff, TIFFTAG_BITSPERSAMPLE, UInt16(tileTrait.sampleBits))
         _ = TIFFSetField(tiff, TIFFTAG_SAMPLESPERPIXEL, UInt16(tileTrait.pixelFormat.rawValue))
         _ = TIFFSetField(tiff, TIFFTAG_COMPRESSION, UInt16(COMPRESSION_LZW))
         _ = TIFFSetField(tiff, TIFFTAG_PLANARCONFIG, UInt16(PLANARCONFIG_CONTIG))
         _ = TIFFSetField(tiff, TIFFTAG_ROWSPERSTRIP, UInt32(stripHeight))
-        _ = TIFFSetField(tiff, TIFFTAG_RESOLUTIONUNIT, UInt16(RESUNIT_CENTIMETER))
-        _ = TIFFSetField(tiff, TIFFTAG_XRESOLUTION, Float(1.0 / scanScale * 1000 * 10))
-        _ = TIFFSetField(tiff, TIFFTAG_YRESOLUTION, Float(1.0 / scanScale * 1000 * 10))
+        _ = TIFFSetField(tiff, TIFFTAG_RESOLUTIONUNIT, UInt16(RESUNIT_INCH))
+        _ = TIFFSetField(tiff, TIFFTAG_XRESOLUTION, Float(1.0 / scanScale * 1000 * 25.4))
+        _ = TIFFSetField(tiff, TIFFTAG_YRESOLUTION, Float(1.0 / scanScale * 1000 * 25.4))
 
         switch tileTrait.pixelFormat {
         case .rgb:
@@ -36,76 +32,67 @@ extension Slide {
             _ = TIFFSetField(tiff, TIFFTAG_PHOTOMETRIC, UInt16(PHOTOMETRIC_MINISBLACK))
         }
 
-        let stripCount = Int(ceil(Double(roiHeight) / Double(stripHeight)))
+        // Init with first tile row
+        let encodingPitch = Int(rect.width) * tileTrait.pixelBytes
+        var encodingPixels = [UInt8](repeating: 0, count: stripHeight * encodingPitch)
 
-        for stripIndex in 0..<stripCount {
-            let stripTop = stripIndex * stripHeight
-            let stripHeightActual = min(stripHeight, roiHeight - stripTop)
-            let stripRectY = Int(rect.minY) + stripTop
-            let stripRectX = Int(rect.minX)
-            let stripRectH = stripHeightActual
-            let stripRectW = roiWidth
+        let colMin = Int(floor(rect.minX / CGFloat(tileTrait.size.w)))
+        let colMax = Int(floor(rect.maxX / CGFloat(tileTrait.size.w)))
+        let rowMin = Int(floor(rect.minY / CGFloat(tileTrait.size.h)))
 
-            let tileOriginX =
-                Int(floor(CGFloat(stripRectX) / CGFloat(tileTrait.size.w)))
-                * tileTrait.size.w
-            let tileOriginY =
-                Int(floor(CGFloat(stripRectY) / CGFloat(tileTrait.size.h)))
-                * tileTrait.size.h
+        var pendingPixels: [UInt8] = []
+        let pendingX = colMin * tileTrait.size.w
+        let pendingWidth = (colMax - colMin + 1) * tileTrait.size.w
+        let pendingPitch = pendingWidth * tileTrait.pixelBytes
+        var pendingHeight = 0
 
-            let colMin = Int(floor(CGFloat(stripRectX) / CGFloat(tileTrait.size.w)))
-            let rowMin = Int(floor(CGFloat(stripRectY) / CGFloat(tileTrait.size.h)))
-            let colMax = Int(
-                floor((CGFloat(stripRectX + stripRectW) - 1.0) / CGFloat(tileTrait.size.w)))
-            let rowMax = Int(
-                floor((CGFloat(stripRectY + stripRectH) - 1.0) / CGFloat(tileTrait.size.h)))
+        let offsetXBytes = (Int(rect.minX) - pendingX) * tileTrait.pixelBytes
+        var offsetY = Int(rect.minY) - (rowMin * tileTrait.size.h)
 
-            let tileFrom = TileCoordinate(layer: 0, row: rowMin, col: colMin)
-            let tileTo = TileCoordinate(layer: 0, row: rowMax, col: colMax)
+        let rowMax = Int(floor(rect.maxY / CGFloat(tileTrait.size.h)))
+        var stripNumber = UInt32(0)
+        for row in rowMin...rowMax {
+            let tileFrom = TileCoordinate(layer: 0, row: row, col: colMin)
+            let tileTo = TileCoordinate(layer: 0, row: row, col: colMax)
             guard let pxdata = fetchPixelData(from: tileFrom, to: tileTo) else {
                 throw SlideExportError.insufficientMemoryForPixelData
             }
 
-            let localX = stripRectX - tileOriginX
-            let localY = stripRectY - tileOriginY
-            let localRight = stripRectX + stripRectW - tileOriginX
-            let localBottom = stripRectY + stripRectH - tileOriginY
+            pendingPixels.append(contentsOf: pxdata.pixels)
+            pendingHeight += pxdata.height
+            if offsetY > 0 {
+                pendingPixels.removeFirst(offsetY * pendingPitch)
+                pendingHeight -= offsetY
+                offsetY = 0
+            }
 
-            let startX = max(0, localX)
-            let startY = max(0, localY)
-            let endX = min(pxdata.width, localRight)
-            let endY = min(pxdata.height, localBottom)
-            let bufferHeight = max(0, endY - startY)
-            let leftPad = max(0, -localX)
-            let topPad = max(0, -localY)
-            let stripBytes = rowStride * stripHeightActual
-            var stripPixels = [UInt8](repeating: 0, count: stripBytes)
+            while pendingHeight > 0 && (pendingHeight >= stripHeight || row == rowMax) {
+                let stripHeightActual = min(stripHeight, pendingHeight)
 
-            for y in startY..<endY {
-                let srcRowOffset = y * pxdata.pitch + startX * pixelBytes
-                let dstRowOffset = (y - startY + topPad) * rowStride + leftPad * pixelBytes
-                let copyWidth = max(0, endX - startX)
-                guard copyWidth > 0 else { continue }
-
-                pxdata.pixels.withUnsafeBytes { srcBuf in
-                    guard let srcBase = srcBuf.baseAddress else { return }
-                    memcpy(
-                        &stripPixels[dstRowOffset], srcBase + srcRowOffset, copyWidth * pixelBytes)
+                for y in 0..<stripHeightActual {
+                    let dest = y * encodingPitch
+                    let src = y * pendingPitch + offsetXBytes
+                    encodingPixels.replaceSubrange(
+                        dest..<(dest + encodingPitch),
+                        with: pendingPixels[src..<(src + encodingPitch)])
                 }
-            }
 
-            let writeResult = stripPixels.withUnsafeBytes { srcBuf in
-                guard let srcBase = srcBuf.baseAddress else { return tmsize_t(-1) }
-                return TIFFWriteEncodedStrip(
-                    tiff,
-                    UInt32(stripIndex),
-                    UnsafeMutableRawPointer(mutating: srcBase),
-                    tmsize_t(bufferHeight * rowStride)
-                )
-            }
+                try encodingPixels.withUnsafeBytes { buf in
+                    if let baseAddress = buf.baseAddress,
+                        TIFFWriteEncodedStrip(
+                            tiff, UInt32(stripNumber),
+                            UnsafeMutableRawPointer(mutating: baseAddress),
+                            tmsize_t(stripHeightActual * encodingPitch)
+                        ) > 0
+                    {
+                        stripNumber += 1
+                    } else {
+                        throw SlideExportError.failedToWriteFile
+                    }
+                }
 
-            guard writeResult >= 0 else {
-                throw SlideExportError.failedToWriteFile
+                pendingPixels.removeFirst(stripHeightActual * pendingPitch)
+                pendingHeight -= stripHeightActual
             }
         }
 
